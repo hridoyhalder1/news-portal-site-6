6c18c0ce5723f75af75f8fd6a0e5d8d1acb790ea
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-09-03 13:26:43 +0600

    category

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const loadCategory = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data  = await res.json();
-    displayCategory(data.data);
+    displayCategory(data.data.news_category);
 }
 const displayCategory = categorys =>{
     const categoryContainer = document.getElementById('category-container');
+    categorys.forEach(category =>{
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+            <div class="col">${category.category_name}</div>
+        `;
+        categoryContainer.appendChild(categoryDiv);
+    })
 }
 loadCategory();
