f229de625de99f6fc26d45c1f1d7631fcb3ac8a1
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-09-03 15:53:09 +0600

    card

--- index.js
+++ index.js
@@ -15,3 +15,40 @@ const displayCategory = categorys =>{
     })
 }
 loadCategory();
+
+
+// card fetch
+
+const loadCards = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+    const res = await fetch(url);
+    const data  = await res.json();
+    displayCards(data.data);
+}
+const displayCards = cards =>{
+    const cardsContainer = document.getElementById('cards-container');
+    cards.forEach(card =>{
+        console.log(card);
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML =`
+        <div class="card mb-5">
+            <img src="${card.image_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${card.title}</h5>
+                <p class="card-text">${card.details.slice(0, 200)}...</p>
+            </div>
+            <div >
+                <div class="author p-3 ">
+                    <img src ="${card.author.img}" class=" w-25 rounded-circle">
+                    <h6 class="card-title">${card.author.name}</h6>
+                    <p>Total View: ${card.total_view}</p>
+                    <button class="bg-primary px-3 text-white ">Details</button>
+                </div>
+            </div>
+        </div>
+        `;
+        cardsContainer.appendChild(cardDiv);
+    })
+}
+loadCards();
